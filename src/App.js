db48cd842a4a39a94a8c2e03a12b8da3312b21aa
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-12 19:36:30 -0600

    created the nav component and styled it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Nav />
     </div>
   );
 }
